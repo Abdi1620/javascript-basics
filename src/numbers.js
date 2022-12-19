0da3c4fe1f15c1eb8bb35a5593d9f0f9c7088a85
Abdi1620 <abdirizakissa5@gmail.com>
2022-12-19 18:27:55 +0000

    javascriptbasic exercise

--- src/numbers.js
+++ src/numbers.js
@@ -1,45 +1,77 @@
-function add (a, b) {
+function add(a, b) {
   // your code here
+
+  let sum = a + b
+  return sum
 }
 
-function subtract (a, b) {
+function subtract(a, b) {
   // your code here
+  less = a - b
+  return less
 }
 
-function multiply (a, b) {
+function multiply(a, b) {
   // your code here
+  multiple = a * b
+  return multiple
 }
 
-function divide (a, b) {
+function divide(a, b) {
   // your code here
+  dividend = a / b
+  return dividend
 }
 
-function power (a, b) {
+function power(a, b) {
   // your code here
+  raisedToPower = Math.pow(a, b)
+  return raisedToPower
 }
 
-function round (a) {
+function round(a) {
   // your code here
+  rounded = Math.round(a)
+  return rounded
+
 }
 
-function roundUp (a) {
+function roundUp(a) {
   // your code here
+  return Math.ceil(a)
 }
 
-function roundDown (a) {
+function roundDown(a) {
   // your code here
+  return Math.floor(a)
 }
 
-function absolute (a) {
+function absolute(a) {
   // your code here
+  return Math.abs(a)
 }
 
-function quotient (a, b) {
+function quotient(a, b) {
   // your code here
+  c = a / b
+  if (c > 0) {
+      return Math.floor(c)
+  }
+
+  if (c < 0) {
+      let positiveC = -1 * c // -1 * -4.5= 4.5
+      e = Math.floor(positiveC) // Math.floor(4.5)
+      return -1 * e
+
+  }
+
+
 }
 
-function remainder (a, b) {
+function remainder(a, b) {
   // your code here
+  modulus = a % b
+  return modulus
 }
 
 module.exports = {
